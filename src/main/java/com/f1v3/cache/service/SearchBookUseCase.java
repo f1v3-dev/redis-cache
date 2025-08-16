64c1071f78bbaeabb5b4b67899f042059f35153d
seungjo <f1v3@kakao.com>
2025-08-16 14:57:49 +0900

    fix: 로그 메시지 수정

--- src/main/java/com/f1v3/cache/service/SearchBookUseCase.java
+++ src/main/java/com/f1v3/cache/service/SearchBookUseCase.java
@@ -27,13 +27,13 @@ public class SearchBookUseCase {
             cacheManager = "cacheManager"
     )
     public SearchBookResponse search(String query, int page) {
-        log.info("Searching books with cache for query: {}, page: {}", query, page);
+        log.info("외부 API를 직접 호출합니다. query: {}, page: {}", query, page);
         return SearchBookResponse.from(searchBookAdapter.search(query, page));
     }
 
     public SearchBookResponse searchWithoutCache(String query, int page, Throwable e) {
-        log.warn("Fallback triggered for search without cache due to: {}", e.getMessage());
-        log.info("Searching books without cache for query: {}, page: {}", query, page);
+        log.warn("Fallback 이 다음과 같은 오류로 인해 활성화 됨 : {}", e.getMessage());
+        log.info("레디스를 사용할 수 없어 외부 API를 호출합니다. query: {}, page: {}", query, page);
         return SearchBookResponse.from(searchBookAdapter.search(query, page));
     }
 }
