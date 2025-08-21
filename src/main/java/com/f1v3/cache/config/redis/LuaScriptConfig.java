a04fb232ec921702c78554e130f2fc72e14c1454
seungjo <f1v3@kakao.com>
2025-08-22 00:15:19 +0900

    feat: LuaScript Configuration

---
+++ src/main/java/com/f1v3/cache/config/redis/LuaScriptConfig.java
@@ -0,0 +1,34 @@
+package com.f1v3.cache.config.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.scripting.support.ResourceScriptSource;
+
+import java.util.List;
+
+/**
+ * Redis Lua Script Configuration for Cache Stampede Prevention.
+ *
+ * @author Seungjo, Jeong
+ */
+@Configuration
+public class LuaScriptConfig {
+
+    @Bean
+    public DefaultRedisScript<List> cacheGetRedisScript() {
+        DefaultRedisScript<List> redisScript = new DefaultRedisScript<>();
+        redisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource("lua/per_cache_get.lua")));
+        redisScript.setResultType(List.class);
+        return redisScript;
+    }
+
+    @Bean
+    public DefaultRedisScript<String> cacheSetRedisScript() {
+        DefaultRedisScript<String> redisScript = new DefaultRedisScript<>();
+        redisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource("lua/per_cache_set.lua")));
+        redisScript.setResultType(String.class);
+        return redisScript;
+    }
+}
